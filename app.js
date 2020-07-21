b833f145d086c0111105c3ff3dee5fb785b8f84f
shhu21 <hu.sara21@gmail.com>
2020-07-20 23:25:40 -0700

    adds promises and styles

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/template-page.js');
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const [name, github] = profileDataArgs;
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -129,17 +127,18 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-});
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
